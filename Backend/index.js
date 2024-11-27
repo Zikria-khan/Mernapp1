4e5c3d6e34b1651515140cc54b174936e6d506a6
Practice <coding4443@gmail.com>
2024-11-27 12:46:00 +0500

    changes adding project

---
+++ Backend/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import userRoute from "./routes/user.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./SocketIO/server.js";
+
+dotenv.config();
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+const URI = process.env.MONGODB_URI;
+
+// MongoDB Connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(URI, {
+      useNewUrlParser: true, // Ensures compatibility with modern connection strings
+      useUnifiedTopology: true, // Enables the new connection management engine
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Exit the process if the connection fails
+  }
+};
+
+// Connect to MongoDB
+connectDB();
+
+// Routes
+app.use("/api/user", userRoute);
+app.use("/api/message", messageRoute);
+
+// Start the Server
+server.listen(PORT, () => {
+  console.log(`Server is Running on port ${PORT}`);
+});
